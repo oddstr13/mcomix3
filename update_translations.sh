b7ba160b7967cd318498d055f3ccdb3f57d02ddc
Odd Stråbø <oddstr13@openshell.no>
2020-08-05 05:05:38 +0200

    Update translations

--- update_translations.sh
+++ update_translations.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 ROOTDIR="mcomix"
-VERSION=$(grep VERSION $ROOTDIR/constants.py | sed -e "s/VERSION = //" -e "s/'//g")
+VERSION=$(grep __version__ $ROOTDIR/__init__.py | sed -e "s/__version__\s*=\s*//" -e "s/['\"\s]//g")
 MAINTAINER="NAME@HO.ST"
 
 xgettext -LPython -omcomix.pot -p$ROOTDIR/messages/ -cTRANSLATORS \
     --from-code=utf-8 --package-name=MComix --package-version=${VERSION} \
     --msgid-bugs-address=${MAINTAINER} \
-    $ROOTDIR/*.py $ROOTDIR/archive/*.py $ROOTDIR/library/*.py
+    $ROOTDIR/**.py
 
 for pofile in $ROOTDIR/messages/*/LC_MESSAGES/*.po
 do
+    echo -n "$pofile "
     # Merge message files with master template, no fuzzy matching (-N)
     msgmerge -U --backup=none ${pofile} $ROOTDIR/messages/mcomix.pot
     # Compile translation, add "-f" to include fuzzy strings
